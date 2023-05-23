777df5a0a5d97c80128ab23116412a6879f7ecd1
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-05-23 22:05:21 +0200

    working

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 // selecting elements
+const input = document.querySelector(".app__search--input");
+const searchBtn = document.querySelector(".app__search__btn--search-icon");
+const weather = document.querySelector(".app__main");
 
+const weatherImg = document.querySelector(".app__main__weather--icon");
+const weatherTemp = document.querySelector(".app__main__weather--temp");
+const weatherName = document.querySelector(".app__main__weather--name");
+const sunriseTime = document.querySelector(".app__main__sunrise--time");
+const sunsetTime = document.querySelector(".app__main__sunset--time");
 // app functionality
 class App {
     constructor() {
+        window.addEventListener("beforeunload", this.#reset);
         window.addEventListener("load", this.#getUserLocation);
+        searchBtn.addEventListener("click", this._showWeather);
+    }
+
+    #reset() {
+        input.value = "";
+    }
+
+    _showWeather() {
+        if (input.value === "") return;
+
+        weather.style.marginTop = "5rem";
+        weather.style.height = "50rem";
+
+        setTimeout(() => {
+            weather.style.opacity = 1;
+            weather.style.visibility = "visible";
+        }, 200);
     }
 
     #getUserLocation() {
@@ -12,12 +38,49 @@ class App {
                 const lat = currentPosistion.coords.latitude;
                 const lng = currentPosistion.coords.longitude;
 
-                const coords = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=721a956f72738d6959dce2e545fb8d44`;
+                const coords = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=721a956f72738d6959dce2e545fb8d44&units=metric`;
 
                 fetch(coords)
                     .then((json) => json.json())
                     .then((data) => {
-                        console.log(data.name);
+                        console.log(data);
+                        input.value = data.name;
+
+                        // show weather function----------------------
+                        weather.style.marginTop = "5rem";
+                        weather.style.height = "50rem";
+                        setTimeout(() => {
+                            weather.style.opacity = 1;
+                            weather.style.visibility = "visible";
+                        }, 200);
+
+                        // display data function ------------------
+
+                        // weather img
+                        if (data.weather[0].description === "")
+                            // weather temp
+                            weatherTemp.innerText = `${Math.trunc(
+                                data.main.temp
+                            )}℃`;
+
+                        // weather name
+                        weatherName.innerText = data.weather[0].description;
+
+                        // sunrise time
+                        const sunriseUnix = data.sys.sunrise;
+                        const sunriseUnixDate = new Date(sunriseUnix * 1000);
+                        const sunriseTimeVal =
+                            sunriseUnixDate.toLocaleTimeString("it-IT");
+
+                        sunriseTime.innerText = sunriseTimeVal.slice(0, 5);
+
+                        // sunset time
+                        const sunsetUnix = data.sys.sunset;
+                        const sunsetUnixDate = new Date(sunsetUnix * 1000);
+                        const sunsetTimeVal =
+                            sunsetUnixDate.toLocaleTimeString("it-IT");
+
+                        sunsetTime.innerText = sunsetTimeVal.slice(0, 5);
                     });
             });
         }
@@ -25,18 +88,6 @@ class App {
 }
 
 const weatherApp = new App();
-
-const qrde = document.querySelector(".app__search__btn--search-icon");
-const ap = document.querySelector(".app__main");
-qrde.addEventListener("click", function () {
-    ap.style.marginTop = "5rem";
-    ap.style.height = "50rem";
-
-    setTimeout(() => {
-        ap.style.opacity = 1;
-        ap.style.visibility = "visible";
-    }, 200);
-});
 
 // ---------------------------------------------------------//
 // ask user for location permission
